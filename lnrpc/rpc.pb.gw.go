e281176439427c867acf3cdda310232ff80b4333
BitfuryLightning <lightning@bitfury.com>
2017-03-30 12:10:16 +0300

    Change SubscribeInvoices http outpoint to resolve conflict

--- lnrpc/rpc.pb.gw.go
+++ lnrpc/rpc.pb.gw.go
@@ -1245,7 +1245,7 @@ var (
 
 	pattern_Lightning_LookupInvoice_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "invoices_hash", "r_hash_str"}, ""))
 
-	pattern_Lightning_SubscribeInvoices_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "invoices", "subscribe"}, ""))
+	pattern_Lightning_SubscribeInvoices_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "invoices_subscribe"}, ""))
 
 	pattern_Lightning_DecodePayReq_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "payreq", "pay_req"}, ""))
 
